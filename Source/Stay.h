54f3b5e852fc8314c2a40a57b9f38587c3554c9b
Junichi Tsurukawa <junichi-tsurukawa@cookpad.com>
2018-04-30 13:53:02 +0900

    Add initial project

---
+++ Source/Stay.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stay.
+FOUNDATION_EXPORT double StayVersionNumber;
+
+//! Project version string for Stay.
+FOUNDATION_EXPORT const unsigned char StayVersionString[];
